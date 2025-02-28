15bd812215cfc0e9e49b5b5f23fefa8e9f41a9c6
Gilbert C. Macêdo <gilbertcm139@gmail.com>
2025-02-27 21:01:55 -0300

    Primeiro Commit

---
+++ Projetos/Pag de Login/scripts.js
@@ -0,0 +1,11 @@
+document.getElementById("loginBtn").addEventListener("click", function() {
+    alert("Botão Conectar foi clicado!");
+});
+
+document.getElementById("esq_senha").addEventListener("click", function() {
+    alert("Botão Esqueci a senha foi clicado!")
+})
+
+document.getElementById("cad_user").addEventListener("click", function() {
+    alert("Botão de cadastro de usuario foi clicado!")
+})
